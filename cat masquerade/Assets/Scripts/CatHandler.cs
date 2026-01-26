8dfad4f4d047e33bb56a009592c9a8336b966a05
Mizzo <mizzosaur@Gmail.com>
2026-01-26 13:46:33 +0100

    Settings change
    had to change a setting

--- cat masquerade/Assets/Scripts/CatHandler.cs
+++ cat masquerade/Assets/Scripts/CatHandler.cs
@@ -2,16 +2,37 @@ using UnityEngine;
 
 public class CatHandler : MonoBehaviour
 {
-    public GameObject Mask;
+    public GameObject mask;
+     private Renderer rend;
+    private Color originalColor;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+         rend = GetComponent<Renderer>();
+        originalColor = rend.material.color;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform == mask.transform)
+            {
+                // Mouse is hovering over this object
+                rend.material.color = Color.yellow;
+            }
+            else
+            {
+                rend.material.color = originalColor;
+            }
+        }
+        else
+        {
+            rend.material.color = originalColor;
+        }
     }
 }
