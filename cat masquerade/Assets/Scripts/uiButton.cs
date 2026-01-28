418b61057e4f4086f1e89acecac31d684eb78f84
Mizzo <mizzosaur@Gmail.com>
2026-01-28 10:46:33 +0100

    fixed main menu button

---
+++ cat masquerade/Assets/Scripts/uiButton.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class uiButton : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void press()
+    {
+        print("Button pressed");
+     SceneManager.LoadScene(sceneBuildIndex: 1);
+    }
+}
